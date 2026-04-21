7b39dadac841841523b61e35492f14d191b43387
EmptyOku <sebastian-diaz1@upc.edu.co>
2026-04-21 16:27:38 -0500

    chore: sync correciones updates

--- resources/js/views/admin/SlatesDetailsView.vue
+++
@@ -1,5 +0,0 @@
-<template>
-  <div><h1>La vista forense de revisión. Implementa un diseño de
-     pantalla dividida (Split-View) mostrando la imagen procesada
-      a la izquierda y el formulario con los datos extraídos (y editables) a la derecha.</h1></div>
-</template>
